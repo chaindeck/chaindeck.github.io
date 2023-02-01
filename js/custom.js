8a27b770a091b44120847424beeee38b480fc0fe
Inno <avinash.yadav@techvzero.com>
2023-02-01 16:25:57 +0530

    v2

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,12 @@ function tabChange() {
   next.tab("show");
 }
 function tabChange() {
-  var tabs = $('.nav-tabs > li');
-  var active = tabs.filter('.active');
-  var next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
-  next.tab('show');
-  //console.log('shiv');
+  var tabs = $(".nav-tabs > li");
+  var active = tabs.filter(".active");
+  var next = active.next("li").length
+    ? active.next("li").find("a")
+    : tabs.filter(":first-child").find("a");
+  next.tab("show");
 }
 
 $(".tab-pane").hover(
@@ -35,4 +36,22 @@ $(function () {
     $(this).tab("show");
     tabCycle = setInterval(tabChange, 5000);
   });
+  //
+  $("#kslide").kslide({});
+  //
 });
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("stickyHeader");
+  if (window.pageYOffset >= 46) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+
+const accordian = document.getElementsByClassName("contentBx");
+for (i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
